1365244ffe66bdad4d621065a1c6139d4cdb3a1f
Modestos Kaffes <modkaffes@gmail.com>
2023-02-10 16:22:34 +0100

    Add Nav component

---
+++ src/components/Nav.tsx
@@ -0,0 +1,15 @@
+export default function Nav() {
+  return (
+    <nav className="flex items-end justify-between">
+      <h1 className="text-3xl font-medium">
+        <a href="/">Grid Fibonacci Sequence</a>
+      </h1>
+      <a
+        href="https://github.com/modkaffes/grid-fibonacci-sequence"
+        target="_blank"
+      >
+        GitHub
+      </a>
+    </nav>
+  );
+}
